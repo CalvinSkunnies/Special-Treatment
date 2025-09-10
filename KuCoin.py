4f8177b543f6b3f32c2db65a670f45c882a4d4a3
CalvinSkunnies <calvinsntf@gmail.com>
2025-09-10 14:26:52 +0700

    Update KuCoin.py

--- KuCoin.py
+++ KuCoin.py
@@ -29,7 +29,7 @@ def fetch_kucoin_st_tokens():
             st_tokens = []
             for symbol in symbols:
                 # Convert tags to uppercase and check for 'ST'
-                st_value = symbol.get('st', 'false')
+                st_value = symbol.get('st', '')
                 if st_value == "true" : # This checks for 'ST', 'st', 'St', 'sT', etc.
                     st_tokens.append({
                         'pair': symbol.get('symbol'),
